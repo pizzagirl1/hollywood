471a340c40179edc8e7ab34625a9f98325317880
nadia <reach.nadia.saria@gmail.com>
2023-07-25 13:40:46 -0700

    add fetch trending actor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import hollywood from './images/hollywood.jpg'
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
+const TMDB_TOKEN = `${process.env.TMDB_API}`
+
+const TMDB_URL = 'https://api.themoviedb.org/3'
+
 function App() {
+
+  const [trendingActor, setTrendingActor] = useState()
+
+  const getActorData = (actorName) => {
+    const headers = {
+      accept: 'application/json',
+      Authorization: 'Bearer ' + TMDB_TOKEN
+    }
+    return axios.get(`${TMDB_URL}/trending/person/day`, headers)
+    .then( (response) => console.log(response))
+    .catch( (e) => console.log(e))
+  }
+
+  const fetchTrendingActor = () => {
+    getActorData().then( (response) => setTrendingActor(response))
+  };
+
+  useEffect( () => fetchTrendingActor(), [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={hollywood} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Today's Trending Actors: {trendingActor}
         </p>
         <a
           className="App-link"
