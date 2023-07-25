906f8058dbfca0f672aa50c8a263256bdb6f172e
nadia <reach.nadia.saria@gmail.com>
2023-07-25 12:30:43 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import hollywood from './images/hollywood.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={hollywood} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
