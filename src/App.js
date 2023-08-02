922d7f48dfb1dd27baaaa2f4d129ee6a0f4308b1
nadia <reach.nadia.saria@gmail.com>
2023-08-02 11:17:32 -0700

    add fetchMovieCredisForActor

--- src/App.js
+++ src/App.js
@@ -116,6 +116,33 @@ function App() {
     // setPopularActors(fetchAllPopularPeople().filter(isMarginalizedGender))
   };
   
+  const fetchMovieCreditsForActor = (actorId) => {
+    const convertFromAPI = (movie) => {
+      return {
+        id: movie.id,
+        title: movie.name,
+        imagePath: person.poster_path,
+        type: 'Movie'
+      }}
+
+    const options = {
+    method: 'GET',
+    url: `${TMDB_URL}/person/${actorId}`,
+    params: {page: page},
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer ' + TMDB_TOKEN
+    }
+  };
+  return axios
+    .request(options)
+    .then((response) => {
+      return response.data.results.cast.map(convertFromAPI);})
+    .catch((error) => {
+      console.log("Error during fetchMovieCreditsForActor", error.message);});
+  }
+  }
+
   const rollActors = () => {
     const getThreePeople = (people) => {
       const result = [];
