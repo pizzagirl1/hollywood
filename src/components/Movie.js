98e5de9055e17fd67eb9176040af979146ff28d5
nadia <reach.nadia.saria@gmail.com>
2023-08-03 18:25:39 -0700

    add default image to Movie and adjust AssetList CSS to align baseline

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,14 @@
 import React from "react";
+import noPhoto from '../images/noPhoto.png'
 import './Actor.css'
 
 const Movie = ({id, title, imagePath, cast}) => {
+
+    const image = imagePath !== null ? ` https://image.tmdb.org/t/p/w500/${imagePath}` : noPhoto;
+
     return (
         <div>
-            <img className="image" src={`https://image.tmdb.org/t/p/w500/${imagePath}`} alt={title}/>
+            <img className="image" src={image} alt={title}/>
             <p>{title}</p>
         </div>
     )
