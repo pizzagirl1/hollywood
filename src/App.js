26b0c41700817964f4edf3492f2b43486929d738
nadia <reach.nadia.saria@gmail.com>
2023-08-08 13:19:53 -0700

    continue polish

--- src/App.js
+++ src/App.js
@@ -210,6 +210,7 @@ function App() {
     setStartingThree(tempActors)
     setGoalActors([goalActors[1], goalActors[0]])
     setChain([])
+    
   }
 
   const chainDisplayArray = [
@@ -277,7 +278,7 @@ function App() {
     const message = `You connected ${goalActors[0].name} to ${goalActors[1].name}!` 
     window.alert(`${message} \n\n${successfulChainArrayText}`)
     startGame()
-    // setResultFromSearch(defaultEmptyActorObject)
+
   }
 
   const onClickSetResultFromSearch = (data) => {
