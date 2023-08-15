71a47fdd3d8ea2677d3eb1f46890040354ad51ff
nadia <reach.nadia.saria@gmail.com>
2023-08-15 11:55:59 -0700

    add js to index.html for dismissal

--- src/App.js
+++ src/App.js
@@ -358,15 +358,15 @@ const App = () => {
         <img src={hollywood} className="App-logo" alt="logo" />
       </header>
       <main>
-      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+      <div class="alert alert-warning alert-dismissible show" role="alert">
         <strong>Holy guacamole!</strong> You should check in on some of those fields below.
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
         <div className="container">
-          <div className="alert alert-success alert-dismissible text-left" role="alert">
-            <button type="button" className="close" data-bs-dismiss="alert" aria-label="Close">
+          <div class="alert alert-success alert-dismissible text-left" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
             <p>
